c8eff80939cfa78b0b4102a57a6dde19524bcfb7
David Tulloch <david.tulloch22@gmail.com>
2024-11-27 06:55:09 -0500

    blanqueo abisall

--- pruebaAPI-BD/pruebaAPI-BD/Models/Usuarios.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace pruebaAPI_BD.Models
-{
-    public class Usuarios
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
